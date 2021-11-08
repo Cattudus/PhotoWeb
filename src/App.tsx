a2c1717d31c0df017c770499114699a481154262
cattu <cattuduslol@gmail.com>
2021-11-08 14:17:41 +0100

    feat: My Work

--- src/App.tsx
+++ src/App.tsx
@@ -2,70 +2,93 @@ import React from 'react';
 import './App.css';
 import useWindowDimensions from "./utils/layout/layout";
 import {Header} from "./components/header/header";
-
+import { Gallery } from './components/Gallery/gallery';
 
 
 function App() {
 
-    const { width } = useWindowDimensions();
-    let vh  = '75vh';
-    function bodyPadding(width: any){
-      if(width <= 1400){
-          vh = '85vh';
-          return 'mobile'
-      } else {
-          vh  = '75vh';
-          return 'body'
-      }
+    const {width} = useWindowDimensions();
+    let vh = '75vh';
+
+    function bodyPadding(width: any) {
+        if (width <= 1400) {
+            vh = '85vh';
+            return 'mobile'
+        } else {
+            vh = '75vh';
+            return 'body'
+        }
     }
-  return (
-    <div className="App">
-        <Header/>
-      <img style={{height: vh, width: '100vw',objectFit: 'cover'}}
-           src="https://www.eachotherweddingsandevents.com/test/wp-content/uploads/2020/04/slide_home.jpg"/>
-        <div className={bodyPadding(width)}>
-          <div id="AboutMe" className="about-me">
-            <div style={{paddingTop: '1rem',
-  paddingBottom: '1rem'}}>
-              <div className="vertical-line"></div>
+
+    return (
+        <div className="App">
+            <Header/>
+            <img style={{height: vh, width: '100vw', objectFit: 'cover'}}
+                 src="https://www.eachotherweddingsandevents.com/test/wp-content/uploads/2020/04/slide_home.jpg"/>
+            <div className={bodyPadding(width)}>
+                <div id="AboutMe" className="about-me">
+                    <div style={{
+                        paddingTop: '1rem',
+                        paddingBottom: '1rem'
+                    }}>
+                        <div className="vertical-line"></div>
+                    </div>
+                    <div className="about-me-header">
+                        About me
+                    </div>
+                    <div className="about-me-text">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec vitae arcu. Curabitur vitae diam
+                        non enim
+                        vestibulum interdum. Aliquam ornare wisi eu metus. Aenean placerat. Aliquam erat volutpat. Et
+                        harum quidem
+                        rerum
+                        facilis est et expedita distinctio. Aliquam erat volutpat. Phasellus et lorem id felis nonummy
+                        placerat.
+                        Duis aute
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Donec quis
+                        nibh at
+                        felis congue commodo. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu
+                        fugiat nulla
+                        pariatur. Nunc tincidunt ante vitae massa. Class aptent taciti sociosqu ad litora torquent per
+                        conubia
+                        nostra, per
+                        inceptos hymenaeos. Praesent in mauris eu tortor porttitor accumsan. Fusce consectetuer risus a
+                        nunc.
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        paddingTop: '2rem',
+                        paddingBottom: '1.5rem'
+                    }}>
+                        <div className="half-vertical-line">
+                        </div>
+                    </div>
+                </div>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <img style={{height: '20rem', width: '20rem', objectFit: 'cover'}}
+                         src="https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/241123596_5148662648483345_2756332344172430026_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=rLS1JWxM5JQAX9WAVK_&_nc_ht=scontent-prg1-1.xx&oh=477be9ad1e536fdc7cfd30c7e54defb3&oe=61AFA961"/>
+                </div>
+
+                <div id="myWork">
+                    <div className="about-me-header">
+                        My Work
+                    </div>
+                    <Gallery/>
+                </div>
             </div>
-            <div className="about-me-header">
-              About me
+
+
+            <div className="footer">
+                <div>
+                    Created by Petr Procházka :)
+                </div>
             </div>
-            <div className="about-me-text">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec vitae arcu. Curabitur vitae diam non enim
-              vestibulum interdum. Aliquam ornare wisi eu metus. Aenean placerat. Aliquam erat volutpat. Et harum quidem
-              rerum
-              facilis est et expedita distinctio. Aliquam erat volutpat. Phasellus et lorem id felis nonummy placerat.
-              Duis aute
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Donec quis
-              nibh at
-              felis congue commodo. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla
-              pariatur. Nunc tincidunt ante vitae massa. Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per
-              inceptos hymenaeos. Praesent in mauris eu tortor porttitor accumsan. Fusce consectetuer risus a nunc.
-            </div>
-            <div style={{display: 'flex', justifyContent: 'center', paddingTop: '2rem', paddingBottom: '1.5rem'}}>
-              <div className="half-vertical-line">
-              </div>
-            </div>
-          </div>
-          <div style={{display: 'flex', justifyContent: 'center'}}>
-            <img style={{height: '20rem', width: '20rem',objectFit: 'cover'}}
-                 src="https://scontent-prg1-1.xx.fbcdn.net/v/t1.6435-9/241123596_5148662648483345_2756332344172430026_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=rLS1JWxM5JQAX9WAVK_&_nc_ht=scontent-prg1-1.xx&oh=477be9ad1e536fdc7cfd30c7e54defb3&oe=61AFA961"/>
-          </div>
+
         </div>
-
-
-        <div className="footer">
-          <div>
-            Created by Petr Procházka :)
-          </div>
-        </div>
-
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Gallery/gallery.tsx
@@ -0,0 +1,69 @@
+import "./gallery.css"
+import useWindowDimensions from "../../utils/layout/layout";
+
+export const Gallery = () => {
+    const {width} = useWindowDimensions();
+
+    function galleryCss(width: any){
+        if(width <=1550){
+            return 'small-gallery'
+        }else{
+            return 'gallery'
+        }
+    }
+    function getImageSize(width: any){
+        if(width <=1550){
+            return 'small-gallery-image'
+        }else{
+            return 'gallery-image'
+        }
+    }
+    function getImageSizes(width: any){
+        if(width <=1550){
+            return '-small'
+        }else{
+            return ''
+        }
+    }
+    return(
+        <div className={galleryCss(width)}>
+            <div className={'container' + getImageSizes(width)}>
+                <img className={getImageSize(width)} src="https://www.brides.com/thmb/NwnePY2QvJ4n_akPcNjokAYPH6g=/2500x2500/smart/filters:no_upscale()/2020_02_22_emilyalex_0876-ab7e30b96b4f44b9b7e562ad7bebea56.jpg"/>
+                <div className={'overlay' + getImageSizes(width)}>
+                        <div className="text">Wedding Ceremony</div>
+                    </div>
+            </div>
+            <div className={'container' + getImageSizes(width)}>
+                <img className={getImageSize(width)} src="https://image.freepik.com/free-photo/wedding-preparation-dressing-up-bride-wedding-ceremony-front-view-wedding-attire_8353-11483.jpg"/>
+                <div className={'overlay' + getImageSizes(width)}>
+                    <div className="text">Wedding Preparations</div>
+                </div>
+            </div>
+            <div className={'container' + getImageSizes(width)}>
+                <img className={getImageSize(width)}  src="https://www.brides.com/thmb/yQ5WEDP-6gecSmGEF9clYIo5Yxs=/3101x3101/filters:fill(auto,1)/ariel-and-alissa-wedding29-a1766c0c4c35406381bed3dee27d7e1a.jpeg"/>
+
+                <div className={'overlay' + getImageSizes(width)}>
+                    <div className="text">You Two</div>
+                </div>
+            </div>
+            <div className={'container' + getImageSizes(width)}>
+                <img className={getImageSize(width)}  src="https://www.yourperfectweddingphotographer.co.uk/wp-content/uploads/2018/12/YPWP-happy-emotion.jpg"/>
+                <div className={'overlay' + getImageSizes(width)}>
+                    <div className="text">Wedding Emotions</div>
+                </div>
+            </div>
+            <div className={'container' + getImageSizes(width)}>
+                <img className={getImageSize(width)}  src="https://www.thespruce.com/thmb/_lAFWHqMqzdLIo0yfaUyb-L6-mQ=/2123x1415/filters:fill(auto,1)/GettyImages-544490573-5653f4e63df78c6ddf16fdd1.jpg"/>
+                <div className={'overlay' + getImageSizes(width)}>
+                    <div className="text">Wedding Celebration</div>
+                </div>
+            </div>
+            <div className={'container' + getImageSizes(width)}>
+                <img className={getImageSize(width)}  src="https://images.squarespace-cdn.com/content/v1/56aaa8964bf11837b0fa2222/1458638785633-M56UIDF24I7GKGL45SAX/shutterstock_282028181.jpg?format=1000w"/>
+                <div className={'overlay' + getImageSizes(width)}>
+                    <div className="text">Young Ones</div>
+                </div>
+            </div>
+        </div>
+    )
+}
